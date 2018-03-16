5bb1c63055d88a4906f7cea99ea979196e4ef635
Dylan <dylan@vandenbosch.co.za>
2018-03-16 09:54:24 +0200

    broken trying to revert and fix

--- src/renderer/store/store.js
+++ src/renderer/store/store.js
@@ -3,6 +3,17 @@ import Vuex from 'vuex'
 
 import HardwareController from './modules/HardwareController'
 import SerialConnection from './modules/SerialConnection'
+// import ipcRenderer from 'electron'
+const {
+  ipcRenderer
+} = require('electron')
+
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "ping" to main console. If I remove the console log it looses the second pong
+ipcRenderer.send('asynchronous-message', 'ping')
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(arg) // prints "pong"
+})
 
 Vue.use(Vuex)
 
